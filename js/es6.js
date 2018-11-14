667ab99e20d3c63c77db2c7920b4f82d22e2700f
MichalWr88 <maleszewski.geo@gmail.com>
2018-11-14 07:49:14 +0100

    refactor start class formValidate

--- js/es6.js
+++ js/es6.js
@@ -1,14 +1,35 @@
-console.log('test');
-
-class Forms  {
-	constructor(name,text) {
-		this.name = document.getElementById(name);
-		this.InpText = name.querySelector()
+class Forms {
+	constructor(id,controls) {
+		this.form = this.fForm(id);
+		this.controls = this.controls();
+		this.errMessage = [{
+			type:'minLength',
+			lable:'to short'
+		}];
+this.initEvents(this.controls)
+		console.log(this.form);
 	}
-Init(){
-
-}
-
+	fForm(id) {
+		
+		return document.getElementById(id);
+	}
+	controls(){
+		return this.form.querySelectorAll('.elem__input')
+	}
+initEvents(array){
+array.forEach((elem)=>{
+elem.addEventListener('input',()=>{
+	console.log(elem.checkValidity());
+	console.log(elem.validity);
+	elem.setCustomValidity("I expect an e-mail, darling!");
 	
+},false)
+})
 }
+	validate(input){
+return input.checkValidity()
+	}
+
+}
+const registerForm = new Forms('register')
 
